76af5370395bd812e0bd5065a552b0da4181b3f9
wee <weehong_1124@outlook.com>
2024-12-04 21:54:14 +0800

    first commit

--- AnonXMusic/__init__.py
+++ AnonXMusic/__init__.py
@@ -1,13 +1,13 @@
 from AnonXMusic.core.bot import Anony
 from AnonXMusic.core.dir import dirr
-from AnonXMusic.core.git import git
+# from AnonXMusic.core.git import git
 from AnonXMusic.core.userbot import Userbot
 from AnonXMusic.misc import dbb, heroku
 
 from .logging import LOGGER
 
 dirr()
-git()
+# git()
 dbb()
 heroku()
 
